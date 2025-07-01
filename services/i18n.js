2b8987886649481111162246dcebaa4e64ac417a
NicolasCastela <nickcastela@hotmail.com>
2025-07-01 17:28:05 -0400

    test: testing index.html

---
+++ services/i18n.js
@@ -0,0 +1,33 @@
+ 
+ const savedLang = localStorage.getItem('language') || 'pt';
+    const folderName = window.location.pathname.split('/').slice(-2, -1)[0];
+    console.log('foldername:', folderName)
+    const pageName = window.location.pathname
+    .split('/')
+    .pop()
+    .replace('.html', '') || 'index';
+    console.log('foldername:', pageName)
+     if (savedLang !== 'pt') {
+    i18next
+      .use(i18nextHttpBackend)
+      .init({
+        lng: savedLang,
+        debug: false,
+        backend: {
+          loadPath: `../../lang/${folderName}/{{lng}}-${pageName}.json`
+        }
+      }, function (err, t) {
+        document.querySelectorAll('[data-i18n]').forEach(el => {
+          const key = el.getAttribute('data-i18n');
+          el.innerHTML = i18next.t(key);
+        });
+      });
+  }
+
+    function changeLang(lang) {
+        localStorage.setItem('language', lang);
+        location.reload();
+    }
+
+
+  
